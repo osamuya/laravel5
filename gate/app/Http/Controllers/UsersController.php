daaa77f9c3985b881a7f3a8613e7c4fd5446308a
osamu <oosamuuy@gmail.com>
2017-04-23 23:52:53 +0900

    /users/xxxxでルーティングでidを渡すところ

--- gate/app/Http/Controllers/UsersController.php
+++ gate/app/Http/Controllers/UsersController.php
@@ -4,23 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 
-class SampleController extends Controller
+class UsersController extends Controller
 {
     /**
      * 指定ユーザーのプロフィール表示
      *
      * @param  int  $id
      * @return Response
-     */
-    public function index()
-    {
-        return view('sample/sample_ctrl');
-    }
-	
+     */	
 	public function page($id)
     {
 //		echo $id;
-        return view('sample/sample_ctrl_page')->with('id',$id);
+        return view('users/page')->with('id',$id);
     }
 
 }
